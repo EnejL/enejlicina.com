8a7502b0fefa4cc239e6f32cf8150e081fc05d41
Enej Licina <enej.licina@windowslive.com>
2025-05-15 17:30:07 +0200

    configured Vite for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: 'https://github.com/EnejL/enejlicina.com',
   plugins: [
     react(),
     mode === 'development' &&
